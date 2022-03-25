fa900632c302013a67c5aa3423fe500f96393df7
Otavio Espindola <otavioespindola@gmail.com>
2022-03-24 21:28:54 -0300

    Utilizando Módulos em JS

---
+++ Modulos_JS/funcoes.mjs
@@ -0,0 +1,18 @@
+function mostraCidade (nome, cidade) {
+    console.log(`${nome} mora em ${cidade}`);
+}
+
+function mostraIdade(nome, idade){
+     console.log(`${nome} tem ${idade} anos de idade`);
+}
+
+function mostraHobby(nome, hobby){
+    console.log(`${nome} tem o hooby de ${hobby}`);
+    
+}
+
+export {
+    mostraCidade,
+    mostraIdade,
+    mostraHobby
+}
---
+++ Modulos_JS/main.mjs
@@ -0,0 +1,6 @@
+import * as Funcs from "./funcoes.mjs";
+
+Funcs.mostraCidade("Otavio", "Casa Branca");
+Funcs.mostraHobby("Otavio", "adquirir novos hábitos");
+Funcs.mostraIdade("Otavio", "35")
+
