dbe677f936e4112e9f50327e723888d84bd3777f
Otavio Espindola <otavioespindola@gmail.com>
2022-03-31 22:43:33 -0300

    adicionado rotas para info dos cursos utilizando variaveis como parametros

--- course-manager/src/app/app.module.ts
+++ course-manager/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { StarComponent } from './star/star.component';
 import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
+import { CourseInfoComponent } from './courses/course-info.component';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,7 @@ import { Error404Component } from './error-404/error-404.component';
     ReplacePipe,
     NavBarComponent,
     Error404Component,
+    CourseInfoComponent
 
   ],
   imports: [
@@ -25,10 +27,13 @@ import { Error404Component } from './error-404/error-404.component';
     FormsModule,
     RouterModule.forRoot([
       {
-        path: '', redirectTo: 'courses', pathMatch: 'full'
+        path: 'courses', component: CourseListComponent
       },
       {
-        path: 'courses', component: CourseListComponent
+        path: 'courses/info/:id', component: CourseInfoComponent
+      },
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
       },
       {
         path: '**', component: Error404Component
