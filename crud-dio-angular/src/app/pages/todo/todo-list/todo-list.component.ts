38e7f755076a99b3dca1eb0dbf664a4bf1d00f1e
Otavio Espindola <otavioespindola@gmail.com>
2022-04-07 13:36:14 -0300

    Feito card de adição de tarefas e metodo para adicionar um item a lista

--- crud-dio-angular/src/app/pages/todo/todo-list/todo-list.component.ts
+++ crud-dio-angular/src/app/pages/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/models/todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  
+  todos: Array<Todo> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  addTodo(title: string){
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false))
+    console.log(this.todos)
   }
 
 }
