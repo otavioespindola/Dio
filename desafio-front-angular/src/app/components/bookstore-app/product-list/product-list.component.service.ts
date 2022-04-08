6d178000cc3fe45949e77af8ad4e65a8a1e5363e
Otavio Espindola <otavioespindola@gmail.com>
2022-04-08 11:21:09 -0300

    Criado desafio de projeto Angular

---
+++ desafio-front-angular/src/app/components/bookstore-app/product-list/product-list.component.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Book } from "./model/Book";
+
+@Injectable()
+
+export class BookService{
+    private url = 'https://sheet.best/api/sheets/1b1e21d7-ee73-4879-8107-7c9500bac61e';
+
+    httpOptions = {
+        Headers: new HttpHeaders({'content-type': 'application/json'})
+    }
+
+    constructor(private http:HttpClient){}
+
+    getBook(){
+        return this.http.get(this.url)
+    }
+}
